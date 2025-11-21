9db2546de4f92d9085994877c242274315a4046c
Meradita <meraditaw@gmail.com>
2025-11-21 15:54:45 +0700

    feat: add NotificationBinding to setup service and controller

--- lib/presentation/bindings/notification_binding.dart
+++ lib/presentation/bindings/notification_binding.dart
@@ -1,14 +1,14 @@
 import 'package:get/get.dart';
-import 'package:luarsekolah/data/providers/notification_service.dart'; 
-import 'package:luarsekolah/presentation/controllers/main_controller.dart'; 
+import 'package:luarsekolah/data/providers/notification_service.dart';
+import 'package:luarsekolah/presentation/controllers/main_controller.dart';
 
 class MainBinding extends Bindings {
   @override
   void dependencies() {
-    // 1. Daftarkan Service FCM sebagai permanent (Wajib)
-    Get.put<NotificationService>(NotificationService(), permanent: true); 
-    
-    // 2. Daftarkan Controller
+    // Daftarkan NotificationService sebagai permanent
+    Get.put<NotificationService>(NotificationService(), permanent: true);
+
+    // Daftarkan MainController
     Get.lazyPut<MainController>(() => MainController());
   }
 }
